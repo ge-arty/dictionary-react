10464ec8dbdbce0e0eef80c33310683bcdc7c5a1
ge-arty <george.arutinyan@gmail.com>
2023-04-02 12:21:38 +0400

    test

--- src/utils/getAudioUrl.js
+++ src/utils/getAudioUrl.js
@@ -2,6 +2,7 @@ export const getAudioUrl = (data) => {
   let foundAudio = false;
   if (data) {
     for (let i = 0; i < data.phonetics.length; i++) {
+      console.log(data.phonetics[i]); // add this line
       if (data.phonetics[i].audio !== "") {
         foundAudio = data.phonetics[i].audio;
         break;
