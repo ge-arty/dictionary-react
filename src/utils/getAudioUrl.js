aa421d3c2ce4a717a870abf99a31cd1b62727235
ge-arty <george.arutinyan@gmail.com>
2023-04-02 12:16:33 +0400

    fix

--- src/utils/getAudioUrl.js
+++ src/utils/getAudioUrl.js
@@ -1,14 +1,16 @@
 export const getAudioUrl = (data) => {
   let foundAudio = false;
-  for (let i = 0; i < data.phonetics.length; i++) {
-    if (data.phonetics[i].audio !== "") {
-      foundAudio = data.phonetics[i].audio;
-      break;
+  if (data) {
+    for (let i = 0; i < data.phonetics.length; i++) {
+      if (data.phonetics[i].audio !== "") {
+        foundAudio = data.phonetics[i].audio;
+        break;
+      }
     }
-  }
-  if (foundAudio) {
-    return foundAudio;
-  } else {
-    return false;
-  }
+    if (foundAudio) {
+      return foundAudio;
+    } else {
+      return false;
+    }
+  } else return foundAudio;
 };
